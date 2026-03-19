948769decbdeb9985ba47cc5e5d0e6402304fe49
Your Name <you@example.com>
2026-03-19 15:50:22 +0200

    wip

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,7 +1,9 @@
 /* в этот файл добавляет скрипты*/
 
 import { initNavMenu } from './nav.js';
+import progressBar from './progress-bar.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initNavMenu();
+  progressBar();
 });
